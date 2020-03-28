c0658ea1d82c3792d3936fe401a48df39a7e86ea
HACKED AD <60751354+Hackes85268@users.noreply.github.com>
2020-03-28 14:17:58 -0600

    Create Menu-2.0.sh

---
+++ Menu-2.0.sh
@@ -0,0 +1,81 @@
+#! / bin / bash
+
+claro
+
+joya instalar lolcat
+
+apto para instalar inodoro -y
+
+claro
+
+eco
+
+echo -e " \ e [1m \ e [36mCREADOR: \ e [31m HACKING PCH "
+
+dormir 3
+
+claro
+
+eco
+
+eco
+
+eco
+
+eco
+
+eco
+
+eco
+
+eco
+
+eco
+
+eco
+
+echo -e " \ e [1m \ e [36m ╔══╗╔══╗╔═╗╔═╦╗╔╗─╔╗╔═╗╔═╦╗╔══╗╔══╗╔═╗
+
+     ║╔╗║╚║║╝║╦╝║║║║║╚╦╝║║╦╝║║║║╚║║╝╚╗╗║║║║
+
+     ║╔╗║╔║║╗║╩╗║║║║╚╗║╔╝║╩╗║║║║╔║║╗╔╩╝║║║║
+
+     ╚══╝╚══╝╚═╝╚╩═╝─╚═╝─╚═╝╚╩═╝╚══╝╚══╝╚═╝
+
+     ────────────────────────────────────── "
+
+eco
+
+echo -e " \ e [0m \ e [1mespere un momento "
+
+dormir 3
+
+termux-open https://www.youtube.com/channel/UCh7BE9ltL8CJNYXYjRFkdLw
+
+claro
+
+eco
+
+echo -e " \ e [1m \ e [36m ╔╗╔╗╔══╗╔═╗╔╦╗╔═╗╔══╗  ╔══╗╔══╗  
+
+║╚╝║║╔╗║║╔╝║╔╝║╦╝╚╗╗║  ║╔╗║╚╗╗║  
+
+║╔╗║║╠╣║║╚╗║╚╗║╩╗╔╩╝║  ║╠╣║╔╩╝║  
+
+╚╝╚╝╚╝╚╝╚═╝╚╩╝╚═╝╚══╝  ╚╝╚╝╚══╝  
+
+─────────────────────  ────────   "
+
+echo -e -n " \ e [1m \ e [32mEscribe tu nombre \ e [0m: \ e [1m \ e [36m "
+
+leer nombre
+
+eco
+
+echo -e " \ e [1m \ e [33mHola \ e [31m $ nombre \ e [33mespero y te funciona el script "
+
+# -----------------------------------------------
+
+dormir 3
+
+bash .menu1.sh
