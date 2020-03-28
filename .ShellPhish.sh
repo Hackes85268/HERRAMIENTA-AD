e9e846414ba6c4ffd652f75bb8614da847ab08c8
HACKED AD <60751354+Hackes85268@users.noreply.github.com>
2020-03-28 14:07:27 -0600

    Create .ShellPhish.sh

---
+++ .ShellPhish.sh
@@ -0,0 +1,47 @@
+#! / bin / bash
+
+claro
+
+eco
+
+echo -e " \ e [1m ╔╗╔╗╔══╗╔═╗╔╦╗╔═╗╔══╗  ╔══╗╔══╗  
+
+║╚╝║║╔╗║║╔╝║╔╝║╦╝╚╗╗║  ║╔╗║╚╗╗║  
+
+║╔╗║║╠╣║║╚╗║╚╗║╩╗╔╩╝║  ║╠╣║╔╩╝║  
+
+╚╝╚╝╚╝╚╝╚═╝╚╩╝╚═╝╚══╝  ╚╝╚╝╚══╝  
+
+─────────────────────  ──────── "  | lolcat
+
+eco
+
+echo -e -n " \ e [1m \ e [32mEscribe la contraseña: \ e [36m "
+
+leer contraseña
+
+case  $ contraseña  en
+
+HACKED-2020) echo ;
+
+echo -e " \ e [1m \ e [31m 😎BIENVENIDO😎 " ;
+
+dormir 4
+
+cd ;
+
+cd HACKED-AD ;
+
+git clone https://github.com/thelinuxchoice/shellphish
+
+bash shellphish.sh ;;
+
+* ) echo -e " \ e [1m \ e [31mPassword incorrecto "
+
+dormir 2
+
+leer -p " Reintentar "
+
+bash " $ 0 " ;;
+
+esac
