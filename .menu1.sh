d97c31ad416f774d7738007f8fbbc209c59f7a44
HACKED AD <60751354+Hackes85268@users.noreply.github.com>
2020-03-28 14:11:17 -0600

    Create .menu1.sh

---
+++ .menu1.sh
@@ -0,0 +1,197 @@
+claro
+
+eco
+
+echo -e " \ e [1m \ e [36m╔╗╔╗╔══╗╔═╗╔╦╗╔═╗╔══╗  ╔══╗╔══╗  
+
+║╚╝║║╔╗║║╔╝║╔╝║╦╝╚╗╗║  ║╔╗║╚╗╗║  
+
+║╔╗║║╠╣║║╚╗║╚╗║╩╗╔╩╝║  ║╠╣║╔╩╝║  
+
+╚╝╚╝╚╝╚╝╚═╝╚╩╝╚═╝╚══╝  ╚╝╚╝╚══╝ "
+
+echo -e " \ e [1m \ e [34m ────────────────────────── ────────── \ e [31m [\ e [36m1.0 \ e [31m] \ e [0m "
+
+# ----------------------------------------------
+
+eco
+
+echo -e " \ e [1m \ e [34m ▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃ ▃▃▃▃▃▃▃▃▃▃▃ "
+
+echo -e " \ e [1m \ e [34m █ \ e [31m [\ e [32m01 \ e [31m] \ e [33m = Social-Fish \ e [31mVIP \ e [1m \ e [34m █ "
+
+echo -e " \ e [1m \ e [34m █ \ e [31m [\ e [32m02 \ e [31m] \ e [33m = ShellPhish \ e [31mVIP \ e [1m \ e [34m █ "
+
+echo -e " \ e [1m \ e [34m █ \ e [31m [\ e [32m03 \ e [31m] \ e [33m = Ojos negros \ e [1m \ e [34m █ "
+
+echo -e " \ e [1m \ e [34m █ \ e [31m [\ e [32m04 \ e [31m] \ e [33m = Weeman \ e [31mVIP \ e [1m \ e [34m █ "
+
+echo -e " \ e [1m \ e [34m █ \ e [31m [\ e [32m05 \ e [31m] \ e [33m = Red-Hawk-info-Gath \ e [1m \ e [34m █ "
+
+echo -e " \ e [1m \ e [34m █ \ e [31m [\ e [32m06 \ e [31m] \ e [33m = D-TECT \ e [1m \ e [34m █ "
+
+echo -e " \ e [1m \ e [34m █ \ e [31m [\ e [32m07 \ e [31m] \ e [33m = Termux-lazy-Script \ e [31mVIP \ e [1m \ e [34m █ "
+
+echo -e " \ e [1m \ e [34m █ \ e [31m [\ e [32m08 \ e [31m] \ e [33m = Pureblood-Framework \ e [1m \ e [34m █ "
+
+echo -e " \ e [1m \ e [34m █ \ e [31m [\ e [32m09 \ e [31m] \ e [33m = ReconDog-Tool \ e [1m \ e [34m █ "
+
+echo -e " \ e [1m \ e [34m █ \ e [31m [\ e [32m10 \ e [31m] \ e [33m = MAS \ e [1m \ e [34m █ "
+
+echo -e " \ e [1m \ e [34m █▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃ ▃▃▃▃▃▃▃▃▃▃█ "
+
+eco
+
+echo -e -n " \ e [1m \ e [34m [\ e [31m! \ e [34m] \ e [0m \ e [1mOPCION \ e [35m> \ e [31m> \ e [36m "
+
+leer numero
+
+caso  $ numero  en
+
+01 | 1) bash .Social-Fish2.sh ;;
+
+02 | 2) bash .ShellPhish.sh ;;
+
+03 | 3) claro ;
+
+cd ;
+
+apt install curl -y ;
+
+mkdir HACKED-AD ;
+
+cd HACKED-AD ;
+
+git clone https://github.com/thelinuxchoice/blackeye
+
+cd blackeye ;
+
+chmod + x * ;
+
+bash blackeye.sh ;;
+
+04 | 4) bash .Weeman.sh ;;
+
+05 | 5) claro ;
+
+cd ;
+
+apt install php -y ;
+
+mkdir HACKED-AD ;
+
+cd HACKED-AD ;
+
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+
+cd RED_HAWK ;
+
+chmod + x * ;
+
+php rhawk.php ;;
+
+06 | 6) claro ;
+
+cd ;
+
+apt install python2 -y ;
+
+mkdir HACKED-AD ;
+
+cd HACKED-AD ;
+
+git clone https://github.com/shawarkhanethicalhacker/D-TECT
+
+cd D-TECT ;
+
+chmod + x * ;
+
+pip2 solicitudes de instalación ;
+
+python2 d-tect.py ;;
+
+07 | 7) bash .Termux-lazy-Script.sh ;;
+
+08 | 8) claro ;
+
+cd ;
+
+apt install python2 -y ;
+
+apt install python -y ;
+
+pkg instalar php -y ;
+
+apt install wget -y ;
+
+mkdir HACKED-AD ;
+
+cd HACKED-AD ;
+
+git clone https://github.com/cr4shcod3/pureblood
+
+cd sangre pura ;
+
+chmod + x * ;
+
+pip install -r required.txt ;
+
+python2 pureblood.py ;;
+
+09 | 9) claro ;
+
+cd ;
+
+apt install python2 -y ;
+
+mkdir HACKED-AD ;
+
+cd HACKED-AD ;
+
+git clone https://github.com/UltimateHackers/ReconDog
+
+cd ReconDog ;
+
+chmod + x * ;
+
+python2 dog.py ;;
+
+10) claro ;  eco ;  eco ;  eco ; echo -e " \ e [1m \ e [31m 😎muy pronto😎 " ;
+
+leer -p "  " ;
+
+bash " $ 0 " ;;
+
+VIP | vip) claro ;
+
+eco ;
+
+eco ;
+
+eco ;
+
+eco ;
+
+eco ;
+
+eco ;
+
+echo -e " \ e [1m \ e [31m▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃ ▃▃▃▃▃▃▃▃▃▃▃▃ " ;
+
+echo -e " \ e [1m \ e [31m█ \ e [1m \ e [33mUSTED ELIJIO \ e [31m🤑VIP🤑 \ e [33mSE LE MANDARA A WHATSAPP. \ e [1m \ e [31m█ " ;
+
+echo -e " \ e [1m \ e [31m█▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃ ▃▃▃▃▃▃▃▃▃▃▃█ " ;
+
+dormir 3 ;
+
+termux-open https://wa.me/5219993407936 ? texto = Hola% 20vengo% 20del% 20script% 20premium% 20de% 20 * HACKED% 20AD * % 20y% 20solicito% 20el% 20c% C3% B3digo% 20 * vip * .. ;
+
+bash " $ 0 " ;;
+
+* ) eco ;  echo -e "                  🚫ERROR ELIJA VIEN🚫 "
+
+leer -p " "
+
+bash " $ 0 " ;;
+
+esac
