c3e5614268e821311da41a33bafbfaa6b1b2631f
HACKED AD <60751354+Hackes85268@users.noreply.github.com>
2020-03-28 14:09:18 -0600

    Create .Termux-lazy-Script.sh

---
+++ .Termux-lazy-Script.sh
@@ -0,0 +1,55 @@
+#! / bin / bash
+
+claro
+
+eco
+
+echo -e " \ e [1m ╔╗╔╗╔══╗╔═╗╔╦╗╔═╗╔══╗  ╔══╗╔══╗  
+
+║╚╝║║╔╗║║╔╝║╔╝║╦╝╚╗╗║  ║╔╗║╚╗╗║  
+
+║╔╗║║╠╣║║╚╗║╚╗║╩╗╔╩╝║  ║╠╣║╔╩╝║  
+
+╚╝╚╝╚╝╚╝╚═╝╚╩╝╚═╝╚══╝  ╚╝╚╝╚══╝  
+
+─────────────────────  ──────── "  | lolcat
+
+eco
+
+echo -e -n " \ e [1m \ e [32mEscribe la contraseña: \ e [36m "
+
+leer contraseña
+
+case  $ contraseña  en
+
+HACKED-2020) echo ;
+
+echo -e " \ e [1m \ e [31m 😎BIENVENIDO😎 " ;
+
+dormir 4
+
+cd ;
+
+apt install python2
+
+cd HACKED-AD ;
+
+git clone https://github.com/TechnicalMujeeb/Termux-Lazyscript.git
+
+cd Termux-Lazyscript ;
+
+chmod + x * ;
+
+sh setup.sh ;
+
+python2 ls.py ;;
+
+* ) echo -e " \ e [1m \ e [31mPassword incorrecto "
+
+dormir 2
+
+leer -p " Reintentar "
+
+bash " $ 0 " ;;
+
+esac
